7c3ee9832701e71087db98b4110ade22f504a986
aleex798 <35605913+aleex798@users.noreply.github.com>
2021-05-30 18:48:01 +0200

    Imported confusion_matrix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans, AffinityPropagation, Birch
 from sklearn.datasets import load_iris, load_digits, load_breast_cancer, load_wine
 from sklearn.mixture import GaussianMixture
+from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import PCA
 
 plt.style.use('seaborn-whitegrid')
